6aa8c1cd53ad7ca7d4d3987372cc7e8b5316a9bb
ceyhunozyilmaz <sanatsalsaat18@gmail.com>
2024-05-27 00:36:40 +0300

    resumapage finished

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -16,6 +16,7 @@ const about = {
     {
       fieldName: "Phone",
       fieldValue: "(+90) 537 299 20 55",
+      link: "tel:+905372992055",
     },
     {
       fieldName: "Experience",
@@ -28,6 +29,7 @@ const about = {
     {
       fieldName: "Email",
       fieldValue: "ceyhunozylmaz14@hotmail.com",
+      link: "mailto:ceyhunozylmaz14@hotmail.com",
     },
     {
       fieldName: "Freekance",
@@ -187,12 +189,70 @@ const Resume = () => {
               </div>
             </TabsContent>
             {/* SKILLS  */}
-            <TabsContent value="skills" className="w-full">
-              Skills
+            <TabsContent value="skills" className="w-full h-full">
+              <div className="flex flex-col gaap-[30px]">
+                <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                  <h3 className="text-4xl font-bold">{skills.title}</h3>
+                  <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
+                    {skills.description}
+                  </p>
+                </div>
+                <ul className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols- gap-4 xl:gap-[30px]">
+                  {skills.skillList.map((skill, index) => {
+                    return (
+                      <li key={index}>
+                        <TooltipProvider delayDuration={100}>
+                          <Tooltip>
+                            <TooltipTrigger className="w-full h-[150px] bg-[#232329] rounded-xl flex justify-center items-center group">
+                              <div className="text-6xl group-hover:text-accent transition-all duration-300">
+                                {skill.icon}
+                              </div>
+                            </TooltipTrigger>
+                            <TooltipContent>
+                              <p className="capitalize">{skill.name}</p>
+                            </TooltipContent>
+                          </Tooltip>
+                        </TooltipProvider>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
             </TabsContent>
             {/* ABOUT  */}
-            <TabsContent value="about" className="w-full">
-              About
+            {/* ABOUT */}
+            <TabsContent
+              value="about"
+              className="w-full text-center xl:text-left"
+            >
+              <div className="flex flex-col gap-[30px]">
+                <h3 className="text-4xl font-bold">{about.title}</h3>
+                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
+                  {about.description}
+                </p>
+                <ul className="grid grid-cols-1 xl:grid-cols-2 gap-y-6 gap-28 max-w-[620px] mx-auto xl:mx-0">
+                  {about.info.map((item, index) => {
+                    return (
+                      <li
+                        key={index}
+                        className="flex items-center justify-center xl:justify-start gap-4"
+                      >
+                        <span className="text-white/60">{item.fieldName}</span>
+                        {item.link ? (
+                          <a
+                            href={item.link}
+                            className="text-md text-white/60 underline"
+                          >
+                            {item.fieldValue}
+                          </a>
+                        ) : (
+                          <span className="text-md">{item.fieldValue}</span>
+                        )}
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
             </TabsContent>
           </div>
         </Tabs>
