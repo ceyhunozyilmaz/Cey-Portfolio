ff251bbc2285b8df9fe28d48ec25eb2d4bfaa421
ceyhunozyilmaz <sanatsalsaat18@gmail.com>
2024-05-29 18:41:08 +0300

    update

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -125,7 +125,7 @@ const Contact = () => {
               onSubmit={sendEmail}
               className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl"
             >
-              <h3 className="text-4xl text-accent">Let's work together</h3>
+              <h3 className="text-4xl text-accent">Let&apos;s work together</h3>
               <p className="text-white/60">
                 If you would like to work together, please get in touch with us.
               </p>
--- app/page.jsx
+++ app/page.jsx
@@ -10,10 +10,10 @@ const Home = () => {
       <div className="container mx-auto h-full">
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
           {/* TEXT */}
-          <div className="text-center xl:text-left order-2  xl:order-none">
+          <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">Frontend Developer</span>
             <h1 className="h1 mb-6">
-              Hello I'm
+              Hello I&apos;m
               <br /> <span className="text-accent">Ceyhun Özyılmaz</span>
             </h1>
             <p className="max-w-[500px] mb-9 text-white/80">
